29f3f1aae5f32154136bdae06dcb492a1746730a
OmarAshraf1911 <omarashraf.90.5.370@gmail.com>
2024-07-25 20:36:19 +0300

    Upload files

---
+++ src/components/CartCard.js
@@ -0,0 +1,11 @@
+import "./CartCard.css";
+export const CartCard = ({ product }) => {
+  const { name, price, image } = product;
+  return (
+    <div className="cartCard">
+      <img src={image} alt={name} />
+      <p className="productName">{name}</p>
+      <p className="productPrice">${price}</p>
+    </div>
+  );
+};
